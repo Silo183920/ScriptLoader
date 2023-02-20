97cddfd2fd13fa23ccabf71aa93c984a057cad52
HALOxHUB <110672283+HALOxHUB@users.noreply.github.com>
2023-02-20 20:34:10 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -9,7 +9,7 @@ end)
 repeat wait() until game:IsLoaded()
 local PlaceId = game.PlaceId
 if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
-    loadstring(game:HttpGet"https://raw.githubusercontent.com/HALOxHUB/MBNEWUPD18/main/README.md")()
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/HALOxHUB/MBNEWUPD18/main/BloxFruit.lua")()
 elseif PlaceId == 537413528 then
     loadstring(game:HttpGet"https://raw.githubusercontent.com/HALOxHUB/Build-A-Boat/main/README.md")()
 elseif PlaceId == 394506555 then
