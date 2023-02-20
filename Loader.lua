53eaec76095dddf18b7d123299ab5491aa4c0028
HALOxHUB <110672283+HALOxHUB@users.noreply.github.com>
2023-02-20 20:47:29 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -6,7 +6,6 @@ VirtualUser:CaptureController()
 VirtualUser:ClickButton2(Vector2.new())
 end)
 --สคิปทุกฮับมีโอกาสโดนแบน!
-repeat wait() until game:IsLoaded()
 local PlaceId = game.PlaceId
 if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
     loadstring(game:HttpGet"https://raw.githubusercontent.com/HALOxHUB/MBNEWUPD18/main/BloxFruit.lua")()
